96c09e1d33015b4d24d4f26af797a988e5fe305d
sheldon <liu_2648349896@126.com>
2024-02-26 20:58:45 +0800

    update 修复网站结构

--- match-frontend/config/routes.ts
+++ match-frontend/config/routes.ts
@@ -1,47 +1,53 @@
 export default [
   { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
   { path: '/welcome', icon: 'smile', component: './Welcome', name: '欢迎页' },
+
+  { path: '/match', redirect: '/match/home' },
+  { icon: 'table', path: '/match/home', component: './Match/Home', name: '主页' },
   {
-    path: '/match',
-    icon: 'crown',
-    name: '伙伴匹配',
+    icon: 'table',
+    path: '/match/search',
+    component: './Match/search',
+    name: '搜索',
+  },
+  {
+    icon: 'table',
+    path: '/match/team',
+    name: '队伍',
     routes: [
-      { path: '/match', redirect: '/match/home' },
-      { icon: 'table', path: '/match/home', component: './Match/Home', name: '主页' },
-      {
-        icon: 'table',
-        path: '/match/search',
-        component: './Match/search',
-        name: '搜索',
-      },
-      { icon: 'table', path: '/match/person', component: './Match/Person', name: '个人' },
       {
         icon: 'table',
         path: '/match/team',
-        name: '队伍',
-        routes: [
-          {
-            icon: 'table',
-            path: '/match/team',
-            redirect: '/match/team/show'
-          },
-          {
-            icon: 'table',
-            path: '/match/team/show',
-            component: './Match/Team',
-            name: '展示队伍',
-          },
-          {
-            icon: 'table',
-            path: '/match/team/add',
-            component: './Match/Team/AddTeam',
-            name: '添加队伍',
-          },
-        ],
+        redirect: '/match/team/show',
+      },
+      {
+        icon: 'table',
+        path: '/match/team/show',
+        component: './Match/Team',
+        name: '展示队伍',
+      },
+      {
+        icon: 'table',
+        path: '/match/team/add',
+        component: './Match/Team/AddTeam',
+        name: '添加队伍',
       },
     ],
   },
-  { path: '/account/settings', icon: 'smile', component: './Account/Settings', name: '个人设置' },
+  {
+    icon: 'table',
+    path: '/match/person',
+    name: '个人',
+    routes: [
+      { path: '/match/person/home', icon: 'smile', component: './Match/Person', name: '个人主页' },
+      {
+        path: '/match/person/settings',
+        icon: 'smile',
+        component: './Account/Settings',
+        name: '个人设置',
+      },
+    ],
+  },
   {
     path: '/admin',
     icon: 'crown',
